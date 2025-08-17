627017f84826c0048b94fa0aa048375fe4a75ca8
Szymon <stszymek@gmail.com>
2025-08-17 17:17:29 +0200

    beter names

--- unet_core/train_model.py
+++ unet_core/train_model.py
@@ -1,9 +1,8 @@
 from unet_interface import UNET
 
 if __name__ == '__main__':
-    unet = UNET(img_height=256, img_width=256)
+    unet = UNET(img_height=64, img_width=64)
     unet.set_model(in_channels=3, out_channels=1)
-    # unet.set_model(in_channels=3, out_channels=1, name="my_checkpoint")
 
     unet.train_model(
         train_img_dir="../data/shaped/images",
@@ -12,18 +11,9 @@ if __name__ == '__main__':
         val_mask_dir="../data/val/shaped/masks",
         learning_rate=1e-4,
         batch_size=16,
-        num_epochs=150,
+        num_epochs=50,
         num_workers=4,
         pin_memory=True,
         save_predictions=True
     )
-    # outlined_image = unet.find(
-    #     "C:/Users/stszy/PycharmProjects/U-Net-neural-network/data/shaped/images/tile_12544_53760_7.tif")
-    # image_np = np.array(outlined_image)
-    # cv2.imshow("Mask", cv2.cvtColor(image_np, cv2.COLOR_RGB2BGR))
-    # cv2.waitKey(0)
-    # cv2.destroyAllWindows()
-    #
-    # unet = UNET(img_height=50, img_width=50)
-    #
     print("UNET model is trained and ready to use. You can now use the model to make predictions or further train it.")
